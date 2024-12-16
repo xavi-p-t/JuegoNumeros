3cbb6ef6eee64e179a83e72125d929c3ac684183
xavi-p-t <xavi.palma06@gmail.com>
2024-12-16 20:01:34 +0100

    fin

--- app/src/main/java/com/xavi/juegonumeros/MainActivity.java
+++ app/src/main/java/com/xavi/juegonumeros/MainActivity.java
@@ -1,38 +1,92 @@
 package com.xavi.juegonumeros;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+int cont = 0;
+int numRan = 0;
+TextView scrollText; //intentCounter;
+ScrollView sc;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        int numRan = (int) (Math.random()*(100-1)+1);
-        final Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                CharSequence text = "Hello toast!";
-                int duration = Toast.LENGTH_SHORT;
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-                Toast toast = Toast.makeText(MainActivity.this /* MyActivity */, text, duration);
-                toast.show();
-            }
-        });
-    }
+    // Inicialitzem variables
+    numRan = generarNum();
+    scrollText = findViewById(R.id.res);
+    sc = findViewById(R.id.scroll);
+    //intentCounter = findViewById(R.id.intentCounter); // Comptador visual
+    Button button = findViewById(R.id.button);
+
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+    button.setOnClickListener(v -> {
+        EditText et = findViewById(R.id.editTextNumber);
+        String edit = et.getText().toString();
+
+        // Validació de l'entrada
+        if (edit.isEmpty() || !edit.matches("\\d+")) {
+            Toast.makeText(MainActivity.this, "Introdueix un número vàlid!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        int numSel = Integer.parseInt(edit);
+        cont++;
+
+        if (numSel > numRan) {
+            scrollText.append("\nEl número és més petit");
+            showToast("El número és més petit");
+        } else if (numSel < numRan) {
+            scrollText.append("\nEl número és més gran");
+            showToast("El número és més gran");
+        } else {
+            // L'usuari encerta
+            builder.setTitle("Has guanyat!")
+                    .setMessage("Has encertat amb " + cont + " intents.")
+                    .setPositiveButton("Jugar de nou", (dialogInterface, i) -> {
+                        cont = 0;
+                        numRan = generarNum();
+                        scrollText.setText(""); // Esborrem historial
+                        //intentCounter.setText("Intents: 0");
+                    })
+                    .setCancelable(false);
+
+            builder.create().show();
+        }
+
+        // Actualitzem el comptador d'intents
+       // intentCounter.setText("Intents: " + cont);
+
+        // Desplacem el ScrollView fins al final
+        sc.post(() -> sc.fullScroll(View.FOCUS_DOWN));
+    });
 }
+
+public int generarNum() {
+    return (int) (Math.random() * (100 - 1) + 1);
+}
+
+private void showToast(String message) {
+    Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+}
+}
+
